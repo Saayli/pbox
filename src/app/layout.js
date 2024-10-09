c228238678172b7b8ec2c92ba41b33e1075c9e51
sayli <hessabilyas@gmail.com>
2024-10-09 15:55:44 +0200

    init

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,35 +2,40 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+    src: "./fonts/GeistVF.woff",
+    variable: "--font-geist-sans",
+    weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+    src: "./fonts/GeistMonoVF.woff",
+    variable: "--font-geist-mono",
+    weight: "100 900",
 });
 
 export const metadata = {
-  title: "Pbox",
-  description: "Application pour utiliser votre pilulier préféré",
+    title: "Pbox",
+    description: "Application pour utiliser votre pilulier préféré",
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="fr">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} mt-16`}
-      >
-        {children}
-      <header><ul className="nav-list">
-          <li><a href="/catalogue">Pills</a></li>
-          <li><a href="/">Alarmes</a></li>
-          <li><a href="#">Profile</a></li>
-
-      </ul></header>
-      </body>
-    </html>
-  );
+export default function RootLayout({children}) {
+    return (
+        <html lang="fr">
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} min-h-screen w-full pt-16 px-4`}
+        >
+        <div className="container">
+            {children}
+        </div>
+            <header>
+                <div className="nav-list fixed bottom-0 left-0 w-full bg-white p-4 drop-shadow-up">
+                <ul className="flex justify-around">
+                    <li><a href="/catalogue">Pills</a></li>
+                    <li><a href="/">Alarmes</a></li>
+                    <li><a href="#">Profile</a></li>
+                </ul>
+                </div>
+            </header>
+        </body>
+        </html>
+    );
 }
